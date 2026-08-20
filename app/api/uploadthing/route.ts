01ef434c163d27aca751d70638ca841a89588a79
Rushikesh Athare <rushikeshathare02@gmail.com>
2026-08-20 19:52:20 +0530

    Update project

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,11 +1,24 @@
 import { createRouteHandler } from "uploadthing/next";
- 
+
 import { ourFileRouter } from "./core";
- 
+
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
- 
-  // Apply an (optional) custom config:
-  // config: { ... },
+  config: {
+    callbackUrl: "http://localhost:3000/api/uploadthing",
+  },
 });
+
+
+// import { createRouteHandler } from "uploadthing/next";
+ 
+// import { ourFileRouter } from "./core";
+ 
+// // Export routes for Next App Router
+// export const { GET, POST } = createRouteHandler({
+//   router: ourFileRouter,
+ 
+//   // Apply an (optional) custom config:
+//   // config: { ... },
+// });
